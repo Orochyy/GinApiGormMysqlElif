ecc3cb6fc51277dd613be8179b1d9f12a93b568e
Orochyy <hfmysterio619>
2021-12-04 13:22:22 +0200

    25%

---
+++ config/database-config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+func SetupDatabaseConnection() *gorm.DB {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to create a connection to database")
+	}
+	return db
+}
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic("Failed to close connection from database")
+	}
+	dbSQL.Close()
+}
