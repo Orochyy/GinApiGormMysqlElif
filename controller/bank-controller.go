dadf8dbc9d1f69856fb6b5a59fc33ca31beb09c0
Orochyy <yura333619@gmail.com>
2022-01-05 13:57:35 +0200

    add func All

--- controller/bank-controller.go
+++ controller/bank-controller.go
@@ -1,7 +1,30 @@
 package controller
 
-type BnakController interface {
+import (
+	"GinApiGormMysqlElif/entity"
+	"GinApiGormMysqlElif/helper"
+	"GinApiGormMysqlElif/service"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type BankController interface {
 }
 
 type bankController struct {
+	bankService service.BankService
+	jwtService  service.JWTService
+}
+
+func NewBankController(bankServ service.BankService, jwtServ service.JWTService) BankController {
+	return &bankController{
+		bankService: bankServ,
+		jwtService:  jwtServ,
+	}
+}
+
+func (c *bankController) All(context *gin.Context) {
+	var banks []entity.Bank = c.bankService.All()
+	res := helper.BuildResponse(true, "OK", banks)
+	context.JSON(http.StatusOK, res)
 }
