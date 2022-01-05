7f413be81ebd22c23d4641920e397815d50715b2
Orochyy <yura333619@gmail.com>
2022-01-05 12:52:31 +0200

    did functional bank-repository.go

--- repository/bank-repository.go
+++ repository/bank-repository.go
@@ -1,1 +1,47 @@
 package repository
+
+import (
+	"GinApiGormMysqlElif/entity"
+	"gorm.io/gorm"
+)
+
+type BankRepository interface {
+}
+
+type bankConnection struct {
+	connection *gorm.DB
+}
+
+func NewBankRepository(dbConn *gorm.DB) BankRepository {
+	return &bankConnection{
+		connection: dbConn,
+	}
+}
+
+func (db *bankConnection) InsertBank(b entity.Bank) entity.Bank {
+	db.connection.Save(&b)
+	db.connection.Preload("User").Find(&b)
+	return b
+}
+
+func (db *bankConnection) UpdateBank(b entity.Bank) entity.Bank {
+	db.connection.Save(&b)
+	db.connection.Preload("User").Find(&b)
+	return b
+}
+
+func (db *bankConnection) DeleteBank(b entity.Bank) {
+	db.connection.Delete(&b)
+}
+
+func (db *bankConnection) FindBankByID(bankID uint64) entity.Bank {
+	var bank entity.Bank
+	db.connection.Preload("User").Find(&bank, bankID)
+	return bank
+}
+
+func (db *bankConnection) AllBank() []entity.Bank {
+	var banks []entity.Bank
+	db.connection.Preload("User").Find(&banks)
+	return banks
+}
