fa72921add51fc148a2c7f53775a0034704b3068
Orochyy <yura333619@gmail.com>
2022-01-06 16:46:27 +0200

    add func FindByID

--- controller/bank-controller.go
+++ controller/bank-controller.go
@@ -6,6 +6,7 @@ import (
 	"GinApiGormMysqlElif/service"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type BankController interface {
@@ -27,4 +28,22 @@ func (c *bankController) All(context *gin.Context) {
 	var banks []entity.Bank = c.bankService.All()
 	res := helper.BuildResponse(true, "OK", banks)
 	context.JSON(http.StatusOK, res)
+}
+
+func (c *bankController) FindByID(context *gin.Context) {
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		res := helper.BuildErrorResponse("No param id was found", err.Error(), helper.EmptyObj{})
+		context.AbortWithStatusJSON(http.StatusBadRequest, res)
+		return
+	}
+
+	var bank entity.Bank = c.bankService.FindByID(id)
+	if (bank == entity.Bank{}) {
+		res := helper.BuildErrorResponse("Data not found", "No data with given id", helper.EmptyObj{})
+		context.JSON(http.StatusNotFound, res)
+	} else {
+		res := helper.BuildResponse(true, "OK", bank)
+		context.JSON(http.StatusOK, res)
+	}
 }
